b8598c3014f378f4b04f4bc880c052a4b7362a06
Zechen Yang <yangzechen21@mails.ucas.ac.cn>
2023-09-07 14:57:08 +0800

    loader.c: finish first ver of load_task_img()

--- Project1-BootLoader/kernel/loader/loader.c
+++ Project1-BootLoader/kernel/loader/loader.c
@@ -11,5 +11,9 @@ uint64_t load_task_img(int taskid)
      * 2. [p1-task4] load task via task name, thus the arg should be 'char *taskname'
      */
 
-    return 0;
+    /* [p1-task3] load via task id */
+    unsigned mem_addr = 0x52000000 + taskid * 0x10000;
+    unsigned block_id = taskid * 15 + 16;
+    bios_sd_read(mem_addr, 15, block_id);
+    return mem_addr;
 }
