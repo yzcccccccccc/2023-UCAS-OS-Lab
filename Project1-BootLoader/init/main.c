93caaeb1801943b8ff21e35d6bfcc4cdd9ade0e2
Zechen Yang <yangzechen21@mails.ucas.ac.cn>
2023-09-08 14:45:51 +0800

    main.c: add debug info

--- Project1-BootLoader/init/main.c
+++ Project1-BootLoader/init/main.c
@@ -63,6 +63,7 @@ static void init_task_info(void)
     task_num = *(info_ptr);
     kernel_size = *(info_ptr + 4);
 
+    bios_putstr("=======================================\n\r");
     bios_putstr("Task Num: ");
     my_print_int((int)task_num);
     bios_putstr("\n\r");
@@ -70,6 +71,7 @@ static void init_task_info(void)
     bios_putstr("Kernel Size: ");
     my_print_int((int)kernel_size);
     bios_putstr("\n\r");
+    bios_putstr("=======================================\n\r");
 
     // loading APP Info to taskinfo[]
     task_info_t *task_info_ptr;
