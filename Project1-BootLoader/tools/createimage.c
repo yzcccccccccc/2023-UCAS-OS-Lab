5df81b15f03744683466c6f0399ee779cf2b8907
Zechen Yang <yangzechen21@mails.ucas.ac.cn>
2023-09-08 15:01:27 +0800

    createimage.c: add debug info

--- Project1-BootLoader/tools/createimage.c
+++ Project1-BootLoader/tools/createimage.c
@@ -170,7 +170,8 @@ static void create_image(int nfiles, char *files[])
         if (strcmp(*files, "main") == 0){
             app_info_bytes = tasknum * sizeof(task_info_t);
             app_info_offset = phyaddr;
-            printf("=== APP Info bytes: %d ===\n", app_info_bytes);
+            printf("****************** APP Info bytes: %d \n", app_info_bytes);
+            printf("****************** APP Info offset: %d\n", app_info_offset);
             phyaddr += app_info_bytes;
         }
 
